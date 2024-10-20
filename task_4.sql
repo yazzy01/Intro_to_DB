ac6a2ef93ff0b839113e79a0cd87308772d4d700
yazzy01 <rziguiyassir@gmail.com>
2024-10-20 12:57:34 -0700

    Fix: Add query for full description of Books table in task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,6 @@
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA 
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE table_name = 'Books' AND table_schema = 'alx_book_store';
+USE alx_book_store;
+
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
 
