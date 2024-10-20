544e97621467b6b3e72b6913520c804c6e3833d5
yazzy01 <rziguiyassir@gmail.com>
2024-10-20 12:49:47 -0700

    Fix: Ensure specific exception handling with mysql.connector.Error in MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -16,7 +16,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:  # Ensure to catch this specific error
         print(f"Error: {e}")
 
     finally:
