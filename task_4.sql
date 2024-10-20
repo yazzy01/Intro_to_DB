4a8b660a46436150c96dabd671763955637236e1
yazzy01 <rziguiyassir@gmail.com>
2024-10-20 12:59:17 -0700

    Fix: Ensure correct use of INFORMATION_SCHEMA.COLUMNS in task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
 USE alx_book_store;
 
 SELECT COLUMN_NAME, COLUMN_TYPE
-FROM information_schema.COLUMNS
+FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
 
